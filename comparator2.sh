c8f0aa90399d2611358f36584a7de1805a3c53ab
RudyCentipede <80247084+RudyCentipede@users.noreply.github.com>
2023-12-07 12:11:18 +0000

    mod

--- comparator2.sh
+++ comparator2.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 verbose=false
-if [[ "$1" == "-v" ]]; then
+if [[ "$3" == "-v" ]]; then
     verbose=true
-    shift
+
 fi
 
-if [[ $# -ne 2 ]]; then
-    echo "Usage: $0 [-v] file1 file2"
-    exit 1
+if [[ "$verbose" == true ]]; then
+    if [[ $# -ne 3 ]]; then
+        echo "Usage: $0 file1 file2 [-v]"
+        exit 1
+    fi
+else
+    if [[ $# -ne 2 ]]; then
+        echo "Usage: $0 file1 file2 [-v]"
+        exit 1
+    fi
 fi
 
 content1=$(awk -F 'string:' '{print $2}' "$1")
 content2=$(awk -F 'string:' '{print $2}' "$2")
 if [[ "$content1" == "$content2" ]]; then
+    if [[ "$verbose" == "true" ]]; then
+        echo "Contents of $1 and $2 are equal"
+    fi
     exit 0
 else
     if [[ "$verbose" == "true" ]]; then
