34d069aa61e3e49de3993c58b87711cf9081c821
RudyCentipede <80247084+RudyCentipede@users.noreply.github.com>
2023-12-08 18:48:58 +0000

    verbose everywhere

--- comparator2.sh
+++ comparator2.sh
@@ -1,47 +1,65 @@
 #!/bin/bash
 verbose=false
-if [[ "$3" == "-v" ]]; then
-    verbose=true
-fi
 
-if [[ "$verbose" == true ]]; then
-    if [[ $# -ne 3 ]]; then
+if [[ $# == 3 ]]; then
+    if [[ "$1" == "-v" && "$2" != "-v" && "$3" != "-v" ]]; then
+        verbose=true
+        file1="$2"
+        file2="$3"
+
+    elif [[ "$1" != "-v" && "$2" == "-v" && "$3" != "-v" ]]; then
+        verbose=true
+        file1="$1"
+        file2="$3"
+
+    elif [[ "$1" != "-v" && "$2" != "-v" && "$3" == "-v" ]]; then
+        verbose=true
+        file1="$1"
+        file2="$2"
+    else
+        echo "Usage: $0 file1 file2 [-v]"
+        exit 1
+    fi
+elif [[ $# == 2 ]]; then
+    if [[ "$1" != "-v" && "$2" != "-v" ]]; then
+        file1="$1"
+        file2="$2"
+    else
         echo "Usage: $0 file1 file2 [-v]"
         exit 1
     fi
 else
-    if [[ $# -ne 2 ]]; then
-        exit 1
-    fi
+    echo "Usage: $0 file1 file2 [-v]"
+    exit 1
 fi
 
-if ! [ -f "$1" ]; then
+if ! [ -f "$file1" ]; then
     if [[ "$verbose" == "true" ]]; then
         echo "File1 does not exist"
     fi
     exit 1
 fi
 
-if ! [ -f "$2" ]; then
+if ! [ -f "$file2" ]; then
     if [[ "$verbose" == "true" ]]; then
         echo "File2 does not exist"
     fi
     exit 1
 fi
 
-content1=$(sed -n '/string:/,$p' "$1")
+content1=$(sed -n '/string:/,$p' "$file1")
 content1=${content1#*string:}
-content2=$(sed -n '/string:/,$p' "$2")
+content2=$(sed -n '/string:/,$p' "$file2")
 content2=${content2#*string:}
 
 if [[ "$content1" == "$content2" ]]; then
     if [[ "$verbose" == "true" ]]; then
-        echo "Contents of $1 and $2 are equal"
+        echo "Contents of $file1 and $file2 are equal"
     fi
     exit 0
 else
     if [[ "$verbose" == "true" ]]; then
-        echo "Contents of $1 and $2 are not equal"
+        echo "Contents of $file1 and $file2 are not equal"
     fi
     exit 1
 fi
--- test.sh
+++ test.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
+echo -n "file1 equal file2: "
 ./comparator2.sh tests/file1 tests/file2
 if [[ $? -eq 1 ]]; then
-    echo Fail
+    echo False
 else
-    echo OK
+    echo True
 fi
 
+echo -n "file2 equal file3: "
 ./comparator2.sh tests/file2 tests/file3
 if [[ $? -eq 1 ]]; then
-    echo Fail
+    echo False
 else
-    echo OK
+    echo True
 fi
 
+echo -n "file1 equal file3: "
 ./comparator2.sh tests/file1 tests/file3
 if [[ $? -eq 1 ]]; then
-    echo Fail
+    echo False
 else
-    echo OK
+    echo True
 fi
